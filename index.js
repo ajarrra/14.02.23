95682b7d663aa48c609a086032e69f4aa0851d4f
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-14 14:26:23 +0100

    created a loop that counts from one to ten

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function  summ (a , b ,c){
     console.log(a + b +c);
 }
 summ (3, 6, 8);
+
+//обьем прямоугольника
+function volume(x, y, z ){
+    console.log 
+    return 
+} 
+
+
+
+//цикл от 1 до 10
+for ( let i = 1 ; i <= 10; i++){
+    console.log (i);
+}
