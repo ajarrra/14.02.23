58a59328f2edb3889049f402f34bcab27724b0d2
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-14 14:41:29 +0100

    created an array that can calculate the sum of the numbers in the array

--- index.js
+++ index.js
@@ -19,4 +19,11 @@ for (let i = 1; i <= 10; i++) {
 //массив из 10 чисел 
 let example1 = [1, 5, 4, 3, 7, 9, 10, 11, 3, 14];
 
+console.log(example1);
+let result = 0;
+for (let i = 0; i < example1.length; i++) {
+  result = result + example1[i];
+}
+console.log(result);
+
 
